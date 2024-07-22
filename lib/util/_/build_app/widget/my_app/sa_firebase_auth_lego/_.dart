84df3a163652b0fb54566f8b72ee04e88a0add40
june lee <melodysdreamj@gmail.com>
2024-07-22 17:58:11 +0900

    Initial commit

---
+++ lib/util/_/build_app/widget/my_app/sa_firebase_auth_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverSaFirebaseAuthLego(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
